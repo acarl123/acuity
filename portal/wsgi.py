2f5faa08facd898a3b97cbce19de9aa695f8221a
Andrew Carl <acarl@1702003M.local>
2017-03-02 23:28:44 -0500

    changed settings for heroku

--- portal/wsgi.py
+++ portal/wsgi.py
@@ -14,8 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
 
 # Dev
-application = get_wsgi_application()
+# application = get_wsgi_application()
 
 # Prod
-# from dj_static import Cling, MediaCling
-# application = Cling(MediaCling(get_wsgi_application()))
+from dj_static import Cling, MediaCling
+application = Cling(MediaCling(get_wsgi_application()))
